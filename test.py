ee750e2f944cf2829150c90887a4606455fa73ef
Nikhilbhamare11 <nikhil.bhamare2004@gmail.com>
2026-07-08 19:55:26 +0530

    Changed Some Lines

--- test.py
+++ test.py
@@ -16,9 +16,22 @@ loanapp = [[
     2000000,   # commercial_assets_value
     5000000,   # luxury_assets_value
     4000000    # bank_asset_value
-   ]]
+]]
 
-loanrej = [[5,0,1,2000000,30000000,20,350,1000000,500000,2000000,1000000]]
+loanrej = [[
+    5,   # no_of_dependents
+    0,   # education (1 = Graduate)
+    1,   # self_employed (0 = No)
+    2000000,   # income_annum
+    30000000,  # loan_amount
+    20,        # loan_term
+    350,       # cibil_score
+    1000000,   # residential_assets_value
+    500000,   # commercial_assets_value
+    2000000,   # luxury_assets_value
+    1000000    # bank_asset_value
+]]
+
 
 # Prediction
 result = model.predict(loanapp)
@@ -28,4 +41,3 @@ if result[0] == 1:
     print("Loan Approved ✅")
 else:
     print("Loan Rejected ❌")
-
